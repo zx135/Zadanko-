aa4005cac52531d8364138722c5ffad66fc5bf4c
Szymon Kuczynski <szykuc2001@gmail.com>
2021-01-07 15:12:22 +0100

    added -

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,2 +1,34 @@
 //To jest plik na którym bedziemy działać
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    char znak;
+    float l1, l2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> znak;
+
+    cout << "Enter two operands: ";
+    cin >> l1 >> l2;
+
+    switch(znak)
+    {
+        case '+':
+            cout << l1+l2;
+            break;
+
+        case '-':
+            cout << l1 - l2;
+            break;
+        default:
+            cout << "Błędny znak operacji";
+            break;
+
+        
+
+    }
+
+    return 0;
+}
