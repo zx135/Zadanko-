d93c19bd56675055d8bb3369031a2598afa7de06
zx135 <szyruag@gmail.com>
2021-01-07 15:10:00 +0100

    Kalkulatorek
    Baza do naszego kalka

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,2 +1,30 @@
 //To jest plik na którym bedziemy działać
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    char znak;
+    float l1, l2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> znak;
+
+    cout << "Enter two operands: ";
+    cin >> l1 >> l2;
+
+    switch(znak)
+    {
+        case '+':
+            cout << l1+l2;
+            break;
+
+        case '-':
+
+        default:
+            cout << "Błędny znak operacji";
+            break;
+    }
+
+    return 0;
+}
